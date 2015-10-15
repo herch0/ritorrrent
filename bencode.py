ee4c69dc203fbbb177d8cac92f3e8a9b211ff33f
Issam RACHDI <issam.rachdi@gmail.com>
2015-10-15 18:48:32 +0100

    ...

--- bencode.py
+++ bencode.py
@@ -5,11 +5,53 @@ reg_list = re.compile(r'l(.*?)e')
 reg_string = re.compile(r'(\d+):(.*?){\1}')
 reg_integer = re.compile(r'i(\d+)e')
 
+current = '_dict'
+root = None
+current_element = None
+
+def _str(bt, str):
+    print('string')
+
+def _int(bt, num):
+    print('integer')
+
+def _list(bt, lst):
+    print('list')
+
+def _dict(bt, dct):
+    print('dict')
+
+def start(bt):
+    global current, root, current_element
+    print('current', current)
+    if re.match(r'\d', bt.decode('ISO-8859-1')):
+        current = '_str'
+    elif bt == b'i':
+        current = '_int'
+    elif bt == b'l':
+        current = '_list'
+    elif bt == b'd':
+        current = '_dict'
+    func = globals()[current]
+    if root == None:
+        root = func(bt)
+    else:
+        current_element = func(bt)
+
+
 def decode(filename):
+    elements = list()
+    element = list()
     #vérifier si fichier torrent valide
-    #torrent = open(filename, mode='r', encoding='ASCII')
     with open(filename, mode='rb') as torrent:
         bt = torrent.read(1)
         while bt:
-            print(bt)
+            start(bt)
+            if (bt != b'e'):
+                element.append(bt)
+            else:
+                elements.append(element)
+                #print(element)
+                break
+                element = list()
             bt = torrent.read(1)
